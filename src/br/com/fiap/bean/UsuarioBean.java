218276cc8e2ee714920dd0a84f80c81e4e209636
cesarnobre <cesarnobrefilho@gmail.com>
2016-04-28 20:48:41 -0300

    first commit

---
+++ src/br/com/fiap/bean/UsuarioBean.java
@@ -0,0 +1,15 @@
+package br.com.fiap.bean;
+
+public class UsuarioBean {
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
